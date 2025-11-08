acf5cd6be26857cfde7b9a60a726f6f705e8c509
krakrukra <krakrukra@tutanota.com>
2025-11-08 12:57:14 +0300

    payload update

--- extra/payloads/InstallSSLCertificate/website/config/configureApache2.sh
+++ extra/payloads/InstallSSLCertificate/website/config/configureApache2.sh
@@ -3,6 +3,8 @@ sudo cp apache2.conf /etc/apache2/
 sudo cp website.conf /etc/apache2/sites-available/
 sudo cp website-ssl.conf /etc/apache2/sites-available/
 sudo a2enmod ssl
+sudo a2dismod mpm_event
+sudo a2enmod php*
 sudo a2ensite website-ssl.conf
 sudo chown -R www-data:www-data /var/www/website/html
 sudo systemctl restart apache2.service
